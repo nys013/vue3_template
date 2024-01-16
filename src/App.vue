913d420a429ebbd6332eeb58425d8b89ec0b9d53
ningyanshan <ningyanshan@szkingdom.com>
2024-01-16 15:49:52 +0800

    style:项目提交后自动触发格式校验

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,15 @@
 <script setup lang="ts">
-const fn = () =>
- {
-  console.log('hello world');
-}
+const fn = () => {
+  console.log("hello world");
+};
 </script>
 
 <template>
-  <div>
-    my app
-  </div>
+  <div>my app</div>
 </template>
 
 <style scoped lang="scss">
-.box{
- 
- 
- 
+.box {
   background-color: red;
 }
 </style>
