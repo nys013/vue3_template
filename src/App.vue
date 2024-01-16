59a0f688657946c6a655cbe577e729daf5070a58
ningyanshan <ningyanshan@szkingdom.com>
2024-01-16 15:48:45 +0800

    feat:项目初始创建，完成基本配置

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const fn = () =>
+ {
+  console.log('hello world');
+}
+</script>
+
+<template>
+  <div>
+    my app
+  </div>
+</template>
+
+<style scoped lang="scss">
+.box{
+ 
+ 
+ 
+  background-color: red;
+}
+</style>
